aac60cbcdbf61eddf2db882c4b03bc2ab1074eb7
MoAbdalbary27 <108543062+MoAbdalbary27@users.noreply.github.com>
2025-09-05 15:46:01 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,11 @@ export const metadata = {
   description: "A simple FixText clone with Next.js and Tailwind CSS",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+type RootLayoutProps = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={cairo.className}>{children}</body>
